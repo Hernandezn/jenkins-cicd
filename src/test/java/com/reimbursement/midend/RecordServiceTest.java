b634e1f9572a3b3bca493e73057060d0e1883261
hernandezn <hernandezn@live.com>
2020-10-30 00:57:47 -0400

    revised testing to use a dummy employee

--- src/test/java/com/reimbursement/midend/RecordServiceTest.java
+++ src/test/java/com/reimbursement/midend/RecordServiceTest.java
@@ -26,14 +26,14 @@ public class RecordServiceTest {
 		recsvc = new RecordService();
 		
 		UserService usvc = new UserService();
-		employee = usvc.login("employee");
+		employee = usvc.login("TEST_EMPLOYEE");
 		manager = usvc.login("manager");
 	}
 	
 	@Test
 	public void trySaving() {
 		RecordDTO record = new RecordDTO(0, "4321.09", "2004-10-19 10:23:54", "2005-10-19 10:23:54", "Record service tester record",
-				new byte[] {(byte)7, (byte)12}, "buffy", "nietzsche", "APPROVED", "safety");
+				new byte[] {(byte)7, (byte)12}, "TEST_EMPLOYEE", "nietzsche", "APPROVED", "safety");
 		RecordDTO trueRecord = recsvc.save(record, employee);
 		
 		assertEquals(record.getDescription(), trueRecord.getDescription());
